5c9f23433a17c44e7f7e1b418512ec9571fc14ef
David Thomas <david.thomas214@yahoo.com>
2016-01-20 09:27:13 -0800

    finalizing answer for static array

--- Win32-Solution/Homework2_StaticArray/main.cpp
+++ Win32-Solution/Homework2_StaticArray/main.cpp
@@ -14,7 +14,7 @@ struct Point3D {
 
 std::ifstream getFileStream();
 Point3D getNextPoint(std::ifstream &fileResc);
-void displayPoints(const Point3D[]);
+void displayPoints(const Point3D[],int);
 void displayCommands();
 void translate(const Point3D point, int amount);
 void reflect(const Point3D, std::string);
@@ -22,6 +22,7 @@ void reflect(const Point3D, std::string);
 int main() {
 	Point3D points[SIZE];
 	int input = -1;
+	int size = SIZE;
 
 	std::cout << "Welcome to Static Array" << std::endl;
 
@@ -32,12 +33,18 @@ int main() {
 	}
 	
 	//Get the points
-	for (int i = 0; i < SIZE; i++) {
-		points[i] = getNextPoint(fileResc);
+	try {
+		for (int i = 0; i < SIZE; i++) {
+			points[i] = getNextPoint(fileResc);
+		}
+	} catch (int e) {
+		std::cout << "Removed Invalid point from list" << e << std::endl;
+		size--;
 	}
+	
 
 	while (input != 0) {
-		displayPoints(points);
+		displayPoints(points,size);
 		displayCommands();
 		std::cin >> input;
 		if (std::cin.fail()) {
@@ -53,7 +60,7 @@ int main() {
 		switch (input) {
 			case(1):				
 				while (point == NULL || amount == NULL) {
-					std::cout << "Input <point [1-" << SIZE << "]> and <amount>" << std::endl;
+					std::cout << "Input <point [1-" << size << "]> and <amount>" << std::endl;
 					std::cin >> point >> amount;
 					if (std::cin.fail()) {
 						std::cout << "Invalid value" << std::endl;
@@ -62,7 +69,7 @@ int main() {
 						std::cin.ignore(std::numeric_limits<int>::max(), '\n');
 						continue;
 					}
-					if (point < 1 || point > SIZE) {
+					if (point < 1 || point > size) {
 						point = NULL;
 					}
 				}
@@ -71,7 +78,7 @@ int main() {
 			   break;
 			case(2) :
 				while (point == NULL || plane.compare("") == 0) {
-					std::cout << "Input <point [1-" << SIZE << "]> and <plane>" << std::endl;
+					std::cout << "Input <point [1-" << size << "]> and <plane>" << std::endl;
 					std::cin >> point >> plane;
 					if (std::cin.fail()) {
 						std::cout << "Invalid value" << std::endl;
@@ -80,7 +87,7 @@ int main() {
 						std::cin.ignore(std::numeric_limits<int>::max(), '\n');
 						continue;
 					}
-					if (point < 1 || point > SIZE) {
+					if (point < 1 || point > size) {
 						point = NULL;
 					}
 					if (plane.compare("xy") != 0 && plane.compare("yx") != 0 && plane.compare("xz") != 0 && plane.compare("zx") != 0 && plane.compare("zy") != 0 && plane.compare("yz") != 0) {
@@ -102,23 +109,23 @@ std::ifstream getFileStream() {
 }
 
 Point3D getNextPoint(std::ifstream &fileResc) {
-	Point3D Instance = { NULL, NULL, NULL };
-	int x = NULL , y = NULL, z = NULL;
-	try {
-		fileResc >> x >> y >> z;
-		if (x && y && z) {
-			Instance = { x,y,z };
-		}
-	} catch (int e) {
+	Point3D Instance = { 0 };
+	int x, y, z;
+
+	fileResc >> x >> y >> z;
+	if (fileResc.fail()) {
 		std::cout << "Failed to read line" << std::endl;
+		throw 10;
 	}
+
+	Instance = { x,y,z };
 
 	return Instance;
 }
 
-void displayPoints(const Point3D points[]) {
+void displayPoints(const Point3D points[],int size) {
 	std::cout << "Point:" << std::endl;
-	for (int i = 0; i < SIZE; i++) {
+	for (int i = 0; i < size; i++) {
 		std::cout << "\t" << (i + 1) << ") X = " << points[i].x << " Y = " << points[i].y << " Z = " << points[i].z << std::endl;
 	}
 }

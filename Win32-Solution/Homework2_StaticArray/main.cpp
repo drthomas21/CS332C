ddecc010d03e536e9c1976e3219d1b7f92cc3978
David Thomas <david.thomas214@yahoo.com>
2016-01-20 10:03:29 -0800

    fixing deleted func?

--- Win32-Solution/Homework2_StaticArray/main.cpp
+++ Win32-Solution/Homework2_StaticArray/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+#include <limits>
 
 const std::string FILENAME = "./points_data.txt";
 const int SIZE = 7;
@@ -12,7 +13,6 @@ struct Point3D {
 	int z;
 };
 
-std::ifstream getFileStream();
 Point3D getNextPoint(std::ifstream &fileResc);
 void displayPoints(const Point3D[],int);
 void displayCommands();
@@ -26,7 +26,7 @@ int main() {
 
 	std::cout << "Welcome to Static Array" << std::endl;
 
-	std::ifstream fileResc= getFileStream();
+	std::ifstream fileResc= std::ifstream(FILENAME);
 	if (!fileResc.good()) {
 		std::cout << "There is an issue with opening the file at " << FILENAME << std::endl;
 		exit(1);
@@ -100,12 +100,6 @@ int main() {
 
 		}
 	}
-}
-
-std::ifstream getFileStream() {
-	std::ifstream fileResc(FILENAME);
-	return fileResc;
-		
 }
 
 Point3D getNextPoint(std::ifstream &fileResc) {

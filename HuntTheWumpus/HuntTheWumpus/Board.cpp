5a0b55725009fcc335817e32179c6d775340d7d3
David Thomas <david.thomas214@yahoo.com>
2016-01-27 09:05:58 -0800

    finalizing the board

--- HuntTheWumpus/HuntTheWumpus/Board.cpp
+++ HuntTheWumpus/HuntTheWumpus/Board.cpp
@@ -6,6 +6,7 @@
 #include "Wumpus.h"
 #include <math.h>
 #include <cstdlib>
+#include <string>
 
 namespace game {
 	//Static Variables
@@ -14,30 +15,73 @@ namespace game {
 
 	//Private & Protected functions
 	bool const Board::isValidLocation(int x, int y) {
-		return x >= 0 && x < Board::size && y >= 0 && y < Board::size;
+		bool ret = x >= 0 && x < Board::size && y >= 0 && y < Board::size;
+		return ret;
+	}
+	bool const Board::isValidLocation(int x, int y, int s) {
+		bool ret = x >= 0 && x < Board::size && y >= 0 && y < Board::size;
+		if (ret) {
+			for (int i = 0; i < s; i++) {
+				if (this->pieces[i].getPositionX() == x && this->pieces[i].getPositionY() == y) {
+					ret = false;
+					break;
+				}
+			}
+		}
+		return ret;
 	}
 	bool const Board::moveNorth(int id) {
+		bool ret = false;
+		int x = this->pieces[id].getPositionX(), y = this->pieces[id].getPositionY();
+		if (this->isValidLocation(x, y - 1)) {
+			this->placePiece(id, x, y - 1);
+			ret = true;
+		}
 
+		return ret;
 	}
 	bool const Board::moveSouth(int id) {
+		bool ret = false;
+		int x = this->pieces[id].getPositionX(), y = this->pieces[id].getPositionY();
+		if (this->isValidLocation(x, y + 1)) {
+			this->placePiece(id, x, y + 1);
+			ret = true;
+		}
 
+		return ret;
 	}
 	bool const Board::moveEast(int id) {
+		bool ret = false;
+		int x = this->pieces[id].getPositionX(), y = this->pieces[id].getPositionY();
+		if (this->isValidLocation(x + 1, y)) {
+			this->placePiece(id, x + 1, y);
+			ret = true;
+		}
 
+		return ret;
 	}
 	bool const Board::moveWest(int id) {
+		bool ret = false;
+		int x = this->pieces[id].getPositionX(), y = this->pieces[id].getPositionY();
+		if (this->isValidLocation(x - 1, y)) {
+			this->placePiece(id, x - 1, y);
+			ret = true;
+		}
 
+		return ret;
 	}
 
 	void Board::operator =(Board const&) {
-
+		//Do nothing
 	}
 
 	void Board::placePiece(int id, int x, int y) {
-
+		this->pieces[id].setPositionX(x);
+		this->pieces[id].setPositionY(y);
 	}
 
 	void Board::setUp() {
+		int id = 0;
 		if (Board::size > 7) {
 			//Trying to keep the ration of 49 slots : 8 players
 			this->players = static_cast<int>(pow(Board::size, 2) / 6.125);
@@ -50,14 +94,15 @@ namespace game {
 
 		//Place the player
 		x = rand() % Board::size, y = rand() % Board::size;
-		this->pieces[0] = pieces::Player(0, "You", x, y);
+		this->pieces[0] = pieces::Player(id, "You", x, y);
+		id++;
 
 		//Place the Wumpus
 		do {
 			x = rand() % Board::size, y = rand() % Board::size;
-		} while (x != this->pieces[0].getPositionX() && y != this->pieces[0].getPositionY());
-		this->pieces[1] = pieces::Player(1, "Wumpus", x, y);
-
+		} while (!this->isValidLocation(x,y,id));
+		this->pieces[1] = pieces::Player(id, "Wumpus", x, y);
+		id++;
 		
 		int numOfTraps, numOfBats, numOfArrows;
 		if (this->players > 8) {
@@ -71,12 +116,39 @@ namespace game {
 			numOfBats = 2;
 			numOfTraps = 2;
 		}
+		
 		//Place the traps
-		for (int i = 0; i < numOfTraps; i++)
+		for (int i = 0; i < numOfTraps; i++) {
+			int x, y;
+			do {
+				x = rand() % Board::size, y = rand() % Board::size;
+			} while (!this->isValidLocation(x, y, id));
+			std::string name = "Trap " + (i + 1);
+			this->pieces[id] = pieces::Trap(id, name.c_str, x, y);
+			id++;
+		}
 
 		//Place the bats
+		for (int i = 0; i < numOfBats; i++) {
+			int x, y;
+			do {
+				x = rand() % Board::size, y = rand() % Board::size;
+			} while (!this->isValidLocation(x, y, id));
+			std::string name = "Bat " + (i + 1);
+			this->pieces[id] = pieces::Bat(id, name.c_str, x, y);
+			id++;
+		}
 
 		//Place the arrows
+		for (int i = 0; i < numOfArrows; i++) {
+			int x, y;
+			do {
+				x = rand() % Board::size, y = rand() % Board::size;
+			} while (!this->isValidLocation(x, y, id));
+			std::string name = "Arrow " + (i + 1);
+			this->pieces[id] = pieces::Arrow(id, name.c_str, x, y);
+			id++;
+		}
 	}
 
 	//Public functions
@@ -86,7 +158,22 @@ namespace game {
 		}
 	}
 	bool const Board::move(int direction, int id) {
-
+		bool ret = false;
+		switch (direction) {
+			case(Board::NORTH) :
+				ret = this->moveNorth(id);
+				break;
+			case(Board::SOUTH) :
+				ret = this->moveSouth(id);
+				break;
+			case(Board::EAST) :
+				ret = this->moveEast(id);
+				break;
+			case(Board::WEST) :
+				ret = this->moveWest(id);
+				break;
+		}
+		return ret;
 	}
 
 	int const Board::getSize() {
@@ -102,10 +189,14 @@ namespace game {
 	}
 
 	//Operators
-	void Board::operator=(Board const&) {}
+	void Board::operator=(Board const&) {
+		//Do Nothing
+	}
 
 	//Copy Constructor
-	Board::Board(Board const&) {}
+	Board::Board(Board const&) {
+		//Do Nothing
+	}
 	
 	//Constructors
 	Board::Board() {
--- HuntTheWumpus/HuntTheWumpus/Board.h
+++ HuntTheWumpus/HuntTheWumpus/Board.h
@@ -5,28 +5,37 @@ namespace game {
 		enum IDS { EMPTY_SPACE, PLAYER_ID, WAMPUS_ID, TELEPORT_ID, TRAP_ID, ARROW_ID };
 		enum EVENT_RESPONSE { INVALID_MOVE, NOTHING_HAPPENED, KILLED_WAMPUS, ATE_BY_WAMPUS, TELEPORTED, TRIGGERED_TRAP, GOT_ARROW };
 		enum DIRECTIONS { NORTH, WEST, SOUTH, EAST };
-	private:
-		static Board* Instance;
-		void operator =(Board const&);
-		Board(Board const&);
-	protected:
-		Board();
-		Board(Board const&);
-		static int size;
-		int players;
-		int** slots;
-		Piece* pieces;
-		bool const isValidLocation(int, int);
-		bool const moveNorth(int);
-		bool const moveSouth(int);
-		bool const moveEast(int);
-		bool const moveWest(int);
-		void placePiece(int, int, int);
-		void setUp();
-	public:
-		static Board* getInstance();
-		static void setBoardSize(int);
-		bool const move(int, int);
-		int const getSize();
+		private:
+			static Board* Instance;
+			void operator =(Board const&);
+			Board(Board const&);
+		protected:
+			//Variables
+			static int size;
+			int players;
+			int** slots;
+			Piece* pieces;
+
+			//Constructor
+			Board();
+			Board(Board const&);
+
+			//Functions
+			bool const isValidLocation(int, int);
+			bool const isValidLocation(int, int, int);
+			bool const moveNorth(int);
+			bool const moveSouth(int);
+			bool const moveEast(int);
+			bool const moveWest(int);
+			void placePiece(int, int, int);
+			void setUp();
+		public:
+			//Singleton
+			static Board* getInstance();
+			static void setBoardSize(int);
+
+			//Functions
+			bool const move(int, int);
+			int const getSize();
 	};
 }
